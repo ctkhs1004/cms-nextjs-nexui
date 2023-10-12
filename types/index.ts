f73aff100714c4cd2c5515069e3021816d98f041
takahashi-c <cho.takahashi@growtech.co.jp>
2023-10-12 17:48:38 +0900

    add api request utils

--- types/index.ts
+++ types/index.ts
@@ -3,3 +3,15 @@ import {SVGProps} from "react";
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
 };
+
+
+export type SuccessResponse<T> = {
+  status: "success";
+  message?: string;
+  data?: T;
+};
+
+export type ErrorResponse = {
+  status: "error";
+  message: string;
+};
