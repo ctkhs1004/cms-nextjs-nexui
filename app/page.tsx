7e49049335e11e287284eb74bc00948ee3f75c88
munakata.junya <munakata.junya@growtech.co.jp>
2023-10-17 12:15:04 +0900

    demo LoginとKeyのsign inの切り替え用タブのレイアウト修正

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import DemoAuthForm from "@/app/signIn/DemoAuthFrom";
 type Variant = 'KEY' | 'LOGIN';
 export default function SignIn() {
     const [key, setKey] = useState('');
-    const [isToggle, setIsToggle] = useState(false);
     const [variant, setVariant] = useState<Variant>('KEY')
     const router = useRouter();
 
@@ -21,50 +20,55 @@ export default function SignIn() {
         }
     };
 
-    const toggleVariant = useCallback(() =>{
-        if(variant === "KEY"){
+    const toggleVariant = useCallback(() => {
+        if (variant === "KEY") {
             setVariant("LOGIN")
         }
 
-        if(variant === "LOGIN"){
+        if (variant === "LOGIN") {
             setVariant("KEY")
         }
-    },[variant])
+    }, [variant])
 
     return (
-        <div className="min-h-screen flex items-center justify-center ">
-            {variant === "LOGIN" && (
-                <div className="p-8 rounded-lg shadow-md w-96">
-                    <h1 className="text-2xl mb-4 text-center">Demo Login</h1>
-                    <DemoAuthForm />
-                </div>
-            )}
-            {variant === 'KEY' && (
-                <div className="p-8 rounded-lg shadow-md w-96">
-                    <h1 className="text-2xl mb-4 text-center">SignIn</h1>
-                    <Input
-                        isRequired
-                        type="text"
-                        label="pubKey"
-                        className="max-w-xs"
-                        value={key}
-                        onChange={(e) => setKey(e.target.value)}
-                    />
-                    <Button
-                        className="mt-4 w-full bg-blue-500 text-white p-2 rounded "
-                        variant="bordered"
-                        onClick={() => {
-                            handleLoginClick(key);
-                        }}
-                    >
-                        SignIn
-                    </Button>
-                </div>
-            )}
-            <div onClick={toggleVariant}
-                 className="underline cursor-pointer">
-                {variant === "KEY" ? 'Use Demo Login' : 'Sign in with your key'}
+        <div className="min-h-screen flex flex-col items-center justify-center ">
+            <div className="mb-1">
+                {variant === "LOGIN" && (
+                    <div className="p-8 rounded-lg shadow-md w-96">
+                        <h1 className="text-2xl mb-4 text-center">Demo Login</h1>
+                        <DemoAuthForm/>
+                    </div>
+                )}
+                {variant === 'KEY' && (
+                    <div className="p-8 rounded-lg shadow-md w-96">
+                        <h1 className="text-2xl mb-4 text-center">SignIn</h1>
+                        <Input
+                            isRequired
+                            type="text"
+                            label="pubKey"
+                            className="max-w-xs"
+                            value={key}
+                            onChange={(e) => setKey(e.target.value)}
+                        />
+                        <Button
+                            className="mt-4 w-full bg-blue-500 text-white p-2 rounded "
+                            variant="bordered"
+                            onClick={() => {
+                                handleLoginClick(key);
+                            }}
+                        >
+                            SignIn
+                        </Button>
+                    </div>
+                )}
             </div>
+            <div className="flex gap-2 justify-center text-sm  px-2 text-gray-500">
+                <div onClick={toggleVariant}
+                     className="underline cursor-pointer">
+                    {variant === "KEY" ? 'Use Demo Login' : 'Sign in with your key'}
+                </div>
+            </div>
+
         </div>
 
     );
