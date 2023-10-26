47fdc3364c2e336e76646385ac8d6d416ea40d48
munakata.junya <munakata.junya@growtech.co.jp>
2023-10-26 19:05:44 +0900

    Profile 自身の投稿を表示

--- app/Profile/page.tsx
+++ app/Profile/page.tsx
@@ -2,12 +2,16 @@
 
 import NavBar from "@/components/NavBar/NavBar";
 import {Footer} from "@/components/Footer";
-import React from "react";
+import React, { useEffect } from "react";
 import Profile from "@/components/Profile/Profile";
 import ProfileUserContents from "@/components/Profile/ProfileUserContents";
 import { useSession } from "next-auth/react";
 
 const ProfilePage = () => {
+    //useSession -> apiからid -> idをProfileUserContentsに渡す
+    // useEffect(() => {
+    //     const axiosGetUser = await getApi(url.postUserKey)
+    // }, []);
     const session = useSession();
     return (
         <div className="flex justify-center items-center py-7">
